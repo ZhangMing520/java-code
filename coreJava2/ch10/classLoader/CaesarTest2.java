2509c639764b02611ce78a38d4af069ae89b8fc4
zhangming <1749909438@qq.com>
2022-07-25 01:08:43 +0800

    feat: Java核心技术.II ch10 类加载器

---
+++ coreJava2/ch10/classLoader/CaesarTest2.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+/**
+ * @author zhangming
+ * @date 7/24/22 10:41 PM
+ * <p>
+ * encrypts a file using caesar cipher
+ * <p>
+ * 1. javac CaesarTest
+ */
+public class CaesarTest2 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Caesar");
+    }
+
+}
